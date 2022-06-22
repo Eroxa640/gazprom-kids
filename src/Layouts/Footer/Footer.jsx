a9afa1c901dcce5b6a92f90d50674cc0498083fc
Kelishbekov <syrgakkelishbekov@gmail.com>
2022-06-22 17:34:47 +0300

    add faq

--- src/Layouts/Footer/Footer.jsx
+++ src/Layouts/Footer/Footer.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import styles from "./Footer.module.sass";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className={styles.footer_head}>
+      <div className={styles.footer}>
+        <div className={styles.footer_container}>
+          <div className={styles.footer_top}>
+            <p>Газпром - Детям</p>
+            <img className={styles.footer_logo} src="/img/footer_logo.svg" />
+          </div>
+          <hr />
+        </div>
+        <div className={styles.footer_section}>
+          <div className={styles.first_block}>
+            <h5>Адрес</h5>
+            <p>ул. Жайыл-Баатыра 66</p>
+          </div>
+          <div className={styles.second_block}>
+            <h5>Связь</h5>
+            <p>Исмаилова Алтынай Курманбековна</p>
+          </div>
+          <div className={styles.third_block}>
+            <h5>Контактные данные</h5>
+            <p>Email: ailinchoy@gmail.com</p>
+            <hr />
+            <p>Номер: +996 (777) 101 002</p>
+            <hr />
+            <p>Номер: +996 (508) 101 002</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -3,8 +3,20 @@ import styles from "./Faq.module.sass";
 
 function Faq() {
   const [isOpen, setOpen] = useState(false);
+  const [isOpen2, setOpen2] = useState(false);
+  const [isOpen3, setOpen3] = useState(false);
   const Toogle = () => {
     setOpen((isOpen) => {
+      return !isOpen;
+    });
+  };
+  const Toogle2 = () => {
+    setOpen2((isOpen) => {
+      return !isOpen;
+    });
+  };
+  const Toogle3 = () => {
+    setOpen3((isOpen) => {
       return !isOpen;
     });
   };
@@ -16,6 +28,7 @@ function Faq() {
           <div className={styles.accordion}>
             <div className={styles.accordion_item}>
               <button
+                className={styles.accordion_button}
                 onClick={Toogle}
                 id={styles.accordion_button_1}
                 aria-expanded="false"
@@ -23,15 +36,84 @@ function Faq() {
                 <span className={styles.accordion_title}>
                   1. Как связаться с нами?
                 </span>
-                <span className={styles.icon} aria-hidden="true"></span>
+                {isOpen ? (
+                  <span className={styles.math} style={{ color: "black" }}>
+                    -
+                  </span>
+                ) : (
+                  <span className={styles.math} style={{ color: "black" }}>
+                    +
+                  </span>
+                )}
               </button>
+              <hr />
               {isOpen ? (
                 <div className={styles.accordion_content}>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor
-                    pretium viverra suspendisse potenti.
+                    Номер : +996 (777) 101 002 <br />
+                    Email : ailinchoy@gmail.com <br />
+                    Адрес : г.Бишкек, Ленинский район <br />
+                    ж/м Арча-Бешик, <br />
+                    ул.Жайыл-Баатыра 66
+                  </p>
+                </div>
+              ) : null}
+
+              <button
+                className={styles.accordion_button}
+                onClick={Toogle2}
+                id={styles.accordion_button_1}
+                aria-expanded="false"
+              >
+                <span className={styles.accordion_title}>
+                  2. Какие мероприятие уже были
+                </span>
+                {isOpen2 ? (
+                  <span className={styles.math} style={{ color: "black" }}>
+                    -
+                  </span>
+                ) : (
+                  <span className={styles.math} style={{ color: "black" }}>
+                    +
+                  </span>
+                )}
+              </button>
+              <hr />
+              {isOpen2 ? (
+                <div className={styles.accordion_content}>
+                  <p>
+                    У нас проходят множество мероприятий но особенно проходят
+                    мероприятии по тхэквандо GTF
+                  </p>
+                </div>
+              ) : null}
+
+              <button
+                className={styles.accordion_button}
+                onClick={Toogle3}
+                id={styles.accordion_button_1}
+                aria-expanded="false"
+              >
+                <span className={styles.accordion_title}>
+                  3. Какие мероприятия будут в будущем
+                </span>
+                {isOpen3 ? (
+                  <span className={styles.math} style={{ color: "black" }}>
+                    -
+                  </span>
+                ) : (
+                  <span className={styles.math} style={{ color: "black" }}>
+                    +
+                  </span>
+                )}
+              </button>
+              <hr />
+              {isOpen3 ? (
+                <div className={styles.accordion_content}>
+                  <p>
+                    Главное событие этого года будет турнир по тхэквандо GTF
+                    между СНГ, где примут участие только профессиональные бойцы
+                    своих категории
                   </p>
                 </div>
               ) : null}
