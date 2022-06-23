723e6d5e2ec4a0cca4d15124c1f90d682535627a
Erbol <a1darbekov640@gmail.com>
2022-06-23 12:22:11 +0300

    Add header

--- src/components/Headers/Headers.jsx
+++ src/components/Headers/Headers.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import styles from './Headers.module.sass'
 
-function Header() {
-  return <div>Header</div>;
+function Headers() {
+  return (
+    <div className={styles.header}>
+      <div className={styles.header__info}>
+        <div className={styles.section}>
+          <h3 className={styles.section__title}>Наши секции</h3>
+          <p className={styles.section__parag}>У нас есть множество секции которые подойдут именно вам</p>
+        </div>
+        <div className={styles.header__grid}>
+          <div className={styles.item}>
+            <h3 className={styles.item__title}>Танцы</h3>
+          </div>
+          <div className={styles.item}>
+            <h3 className={styles.item__title}>Таэквондо GTF</h3>
+          </div>
+          <div className={styles.item}>
+            <h3 className={styles.item__title}>Борьба (Вольная, Греко-Римская)</h3>
+          </div>
+          <div className={styles.item}>
+            <h3 className={styles.item__title}>Футбол</h3>
+          </div>
+          <div className={styles.item}>
+            <h3 className={styles.item__title}>Бокс</h3>
+          </div>
+          <div className={styles.item}>
+            <h3 className={styles.item__title}>Йога</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Header;
+export default Headers;
