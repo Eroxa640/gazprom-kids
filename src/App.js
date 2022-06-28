1ea769a0837b94831820642e0334e09e108ba457
Kelishbekov <syrgakkelishbekov@gmail.com>
2022-06-28 11:50:51 +0300

    add section block

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import {Route, Routes} from "react-router-dom"
-import Main from './components/Main/Main'
-import NavLayouts from './Layouts/NavLayouts/NavLayouts'
-import './App.sass'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Main from "./components/Main/Main";
+import NavLayouts from "./Layouts/NavLayouts/NavLayouts";
+import "./App.sass";
+import Section from "./components/Section/Section";
 
 function App() {
   return (
     <NavLayouts>
       <Routes>
-        <Route path='/' element={<Main/>} />
+        <Route path="/" element={<Main />} />
+        <Route path="/sections" element={<Section />} />
       </Routes>
     </NavLayouts>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Section.module.sass";
+
+function Section() {
+  return <div></div>;
+}
+
+export default Section;
